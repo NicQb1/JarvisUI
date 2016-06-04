e0a99c76c0fbfaa82edd7db1b0cc1158eaa1c0a9
Nick Hutchison <n_hutchison@yahoo.com>
2016-06-03 23:38:05 -0400

    initial checkin

---
+++ JarvisUI/Form1.Designer.cs
@@ -0,0 +1,61 @@
+﻿namespace JarvisUI
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnSpeechRecognition = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnSpeechRecognition
+            // 
+            this.btnSpeechRecognition.Location = new System.Drawing.Point(74, 35);
+            this.btnSpeechRecognition.Name = "btnSpeechRecognition";
+            this.btnSpeechRecognition.Size = new System.Drawing.Size(130, 23);
+            this.btnSpeechRecognition.TabIndex = 0;
+            this.btnSpeechRecognition.Text = "Start speech recognition";
+            this.btnSpeechRecognition.UseVisualStyleBackColor = true;
+            this.btnSpeechRecognition.Click += new System.EventHandler(this.btnSpeechRecognition_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(282, 255);
+            this.Controls.Add(this.btnSpeechRecognition);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnSpeechRecognition;
+    }
+}
+
---
+++ JarvisUI/Form1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Speech;
+using System.Speech.Recognition;
+
+namespace JarvisUI
+{
+    public partial class Form1 : Form
+    {
+        public SpeechRecognitionEngine sre = new SpeechRecognitionEngine(new System.Globalization.CultureInfo("en-US"));
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSpeechRecognition_Click(object sender, EventArgs e)
+        {
+            
+            //DictationGrammar defaultDictationGrammar = new DictationGrammar();
+            //defaultDictationGrammar.Name = "default dictation";
+            //defaultDictationGrammar.Enabled = true;
+            Choices commands = new Choices();
+            commands.Add(new string[] { "Jarvis", "Hey", "Computer", "Lights" });
+
+            GrammarBuilder gb = new GrammarBuilder();
+            gb.Append(commands);
+
+            // Create the Grammar instance.
+            Grammar g = new Grammar(gb);
+            sre.LoadGrammarAsync(g);
+          //  sre.LoadGrammarAsync(defaultDictationGrammar);
+            sre.SpeechRecognized += new EventHandler<SpeechRecognizedEventArgs>(sre_SpeechRecognized);
+            sre.SetInputToDefaultAudioDevice();
+            sre.RecognizeAsync(RecognizeMode.Multiple);
+        }
+        public void sre_SpeechRecognized(object sender, SpeechRecognizedEventArgs e)
+        {
+            MessageBox.Show("Speech recognized: " + e.Result.Text);
+        }
+    }
+}
